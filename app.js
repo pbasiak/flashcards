2c84298a7ec618ab646edc976309907c4bb203eb
pbasiak <wilczek241@gmail.com>
2020-12-10 20:30:31 +0100

    feat: serve app with express

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("hello world!\n");
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-server.listen(3000);
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
