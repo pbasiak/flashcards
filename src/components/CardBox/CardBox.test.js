0022d5e1d57b7cbed79677f2b71f9a072f8f9ad5
pbasiak <wilczek241@gmail.com>
2021-02-21 17:46:57 +0100

    test: add test for cardbox

---
+++ src/components/CardBox/CardBox.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react"
+import CardBox from "./CardBox"
+
+describe('CardBox', () => {
+    it('renders correctly', () => {
+        render(<CardBox>CardBoxContent</CardBox>);
+
+        expect(screen.getByText('CardBoxContent'));
+    })
+})
