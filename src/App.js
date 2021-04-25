1cc131c6b29e3c8ce5481d4009285f81d6274457
pbasiak <wilczek241@gmail.com>
2021-04-25 14:16:48 +0200

    fix: change error message

--- src/App.js
+++ src/App.js
@@ -18,24 +18,7 @@ import Contact from "./pages/Contact/Contact";
 
 function App() {
   if (!process.env.REACT_APP_BACKEND_URL) {
-    return (
-      <p>
-        Please specify your backend url with the{" "}
-        <a
-          href="https://create-react-app.dev/docs/adding-custom-environment-variables/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          environment variable
-        </a>
-        :<br />
-        <b>REACT_APP_BACKEND_URL</b>.<br />
-        <br />
-        For example launch this app with:
-        <br />
-        <b>REACT_APP_BACKEND_URL=http://localhost:1337 yarn start</b>
-      </p>
-    );
+    return <p>API URL not set.</p>;
   }
 
   if (process?.env?.NODE_ENV === "production") {
